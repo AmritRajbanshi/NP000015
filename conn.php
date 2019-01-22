414dbb2e92e8fd811b9bcff83ecf6984392a8699
AmritRajbanshi <46885043+AmritRajbanshi@users.noreply.github.com>
2019-01-22 14:29:02 +0545

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,8 +1,8 @@
 <?php
-	//$conn = mysqli_connect( 'amrit-np000015.mysql.database.azure.com','amritrajbanshi@amrit-np000015','','ajax' );
-	$conn=mysqli_init(); 
+	$conn = mysqli_connect( 'amrit-np000015.mysql.database.azure.com','amritrajbanshi@amrit-np000015','Dirtyfeat123','AmritCMS_DB' );
+	/*$conn=mysqli_init(); 
 	mysqli_ssl_set($conn, NULL, NULL, {ca-cert filename}, NULL, NULL); 
-	mysqli_real_connect($conn, "amrit-np000015.mysql.database.azure.com", "amritrajbanshi@amrit-np000015", "Dirtyfeat123", "AmritCMS_DB", 3306);
+	mysqli_real_connect($conn, "amrit-np000015.mysql.database.azure.com", "amritrajbanshi@amrit-np000015", "Dirtyfeat123", "AmritCMS_DB", 3306);*/
   // Check connection
   if (mysqli_connect_errno()){
     echo "Connection database failed : " . mysqli_connect_error();
