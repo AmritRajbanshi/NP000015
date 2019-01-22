4fa1b856b2bffebad196f6e4f5af4c746e2144bb
AmritRajbanshi <46885043+AmritRajbanshi@users.noreply.github.com>
2019-01-22 13:49:32 +0545

    update conn
    Azure database setup

--- conn.php
+++ conn.php
@@ -1,5 +1,8 @@
 <?php
-	$conn = mysqli_connect( 'localhost','root','','ajax' );
+	//$conn = mysqli_connect( 'localhost','root','','ajax' );
+	$conn=mysqli_init(); 
+	mysqli_ssl_set($conn, NULL, NULL, {ca-cert filename}, NULL, NULL); 
+	mysqli_real_connect($conn, "amrit-np000015.mysql.database.azure.com", "amritrajbanshi@amrit-np000015", {Dirtyfeat123}, {AmritCMS_DB}, 3306);
   // Check connection
   if (mysqli_connect_errno()){
     echo "Connection database failed : " . mysqli_connect_error();
